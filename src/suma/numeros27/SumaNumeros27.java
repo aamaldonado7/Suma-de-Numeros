00e47255c333def128193d3ede7c06a7e90c9607
aamaldonado7 <almaldonado78@hotmail.com>
2018-07-12 00:51:52 -0600

    This is another project of mine

---
+++ src/suma/numeros27/SumaNumeros27.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma.numeros27;
+
+/**
+ *
+ * @author Alejandro Maldonado
+ */
+public class SumaNumeros27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int numero1 = 4;
+        
+        int numero2 = 5;
+        
+        int suma = 0;
+        
+        String mensaje = "La suma es ";
+        
+        suma = numero1 + numero2;
+        
+        System.out.println(mensaje + suma);
+        
+        // TODO code application logic here
+    }
+    
+}
